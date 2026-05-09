236dce0aa8e0aa7bd887269edb1fe18f095448b8
Ada Freya Ahmed <ada@chronovore.dev>
2026-05-09 08:58:47 +0530

    chore: fix knit.gltf analysis

--- Jackdaw.Tests/Cache/IndexParserTests.cs
+++ Jackdaw.Tests/Cache/IndexParserTests.cs
@@ -16,18 +16,18 @@ internal class IndexParserTests {
 
 		Assert.That(result, Has.Length.EqualTo(2));
 
-		Assert.Multiple(() => {
-			Assert.That(result[0].Path.OriginalString, Is.EqualTo("res:/intromovie.txt"));
-			Assert.That(result[0].ResourcePath, Is.EqualTo("a9/a9d1721dd5cc6d54_e6bbb2df307e5a9527159a4c971034b5"));
-			Assert.That(result[0].MD5, Is.EqualTo("e6bbb2df307e5a9527159a4c971034b5"));
-			Assert.That(result[0].Size, Is.EqualTo(9719));
-			Assert.That(result[0].CompressedSize, Is.EqualTo(3312));
+		using var scope = Assert.EnterMultipleScope();
 
-			Assert.That(result[1].Path.OriginalString, Is.EqualTo("res:/videocardcategories.yaml"));
-			Assert.That(result[1].ResourcePath, Is.EqualTo("ab/ab5cde4fbbf82fb6_6a9d6d4c6015616877b77865209c5064"));
-			Assert.That(result[1].MD5, Is.EqualTo("6a9d6d4c6015616877b77865209c5064"));
-			Assert.That(result[1].Size, Is.EqualTo(33591));
-			Assert.That(result[1].CompressedSize, Is.EqualTo(5076));
-		});
+		Assert.That(result[0].Path.OriginalString, Is.EqualTo("res:/intromovie.txt"));
+		Assert.That(result[0].ResourcePath, Is.EqualTo("a9/a9d1721dd5cc6d54_e6bbb2df307e5a9527159a4c971034b5"));
+		Assert.That(result[0].MD5, Is.EqualTo("e6bbb2df307e5a9527159a4c971034b5"));
+		Assert.That(result[0].Size, Is.EqualTo(9719));
+		Assert.That(result[0].CompressedSize, Is.EqualTo(3312));
+
+		Assert.That(result[1].Path.OriginalString, Is.EqualTo("res:/videocardcategories.yaml"));
+		Assert.That(result[1].ResourcePath, Is.EqualTo("ab/ab5cde4fbbf82fb6_6a9d6d4c6015616877b77865209c5064"));
+		Assert.That(result[1].MD5, Is.EqualTo("6a9d6d4c6015616877b77865209c5064"));
+		Assert.That(result[1].Size, Is.EqualTo(33591));
+		Assert.That(result[1].CompressedSize, Is.EqualTo(5076));
 	}
 }
--- Jackdaw.Tests/StartParserTests.cs
+++ Jackdaw.Tests/StartParserTests.cs
@@ -29,23 +29,23 @@ internal class StartParserTests {
 		using var stream = new MemoryStream(Encoding.UTF8.GetBytes(SAMPLE));
 		var result = StartParser.Parse(stream);
 
-		Assert.Multiple(() => {
-			Assert.That(result.Version, Is.EqualTo("21.03"));
-			Assert.That(result.Build, Is.EqualTo("2239025"));
-			Assert.That(result.Codename, Is.EqualTo("EVE-RELEASE"));
-			Assert.That(result.Region, Is.EqualTo("ccp"));
-			Assert.That(result.CryptoPack, Is.EqualTo("CryptoAPI"));
-			Assert.That(result.Sync, Is.EqualTo("2239025"));
-			Assert.That(result.Branch, Is.EqualTo("//eve/branches/release/V21.03"));
-			Assert.That(result.AppName, Is.EqualTo("EVE"));
-			Assert.That(result.UseScriptIndexFiles, Is.EqualTo(true));
-			Assert.That(result.SocketIO, Is.EqualTo("iocp"));
-			Assert.That(result.Server, Is.EqualTo("Tranquility"));
-			Assert.That(result.Edition, Is.EqualTo("premium"));
-			Assert.That(result.Role, Is.EqualTo("client"));
-			Assert.That(result.Aid, Is.EqualTo(false));
-			Assert.That(result.ResFromStuffOnly, Is.EqualTo(false));
-			Assert.That(result.Port, Is.EqualTo(26000));
-		});
+		using var scope = Assert.EnterMultipleScope();
+
+		Assert.That(result.Version, Is.EqualTo("21.03"));
+		Assert.That(result.Build, Is.EqualTo("2239025"));
+		Assert.That(result.Codename, Is.EqualTo("EVE-RELEASE"));
+		Assert.That(result.Region, Is.EqualTo("ccp"));
+		Assert.That(result.CryptoPack, Is.EqualTo("CryptoAPI"));
+		Assert.That(result.Sync, Is.EqualTo("2239025"));
+		Assert.That(result.Branch, Is.EqualTo("//eve/branches/release/V21.03"));
+		Assert.That(result.AppName, Is.EqualTo("EVE"));
+		Assert.That(result.UseScriptIndexFiles, Is.EqualTo(true));
+		Assert.That(result.SocketIO, Is.EqualTo("iocp"));
+		Assert.That(result.Server, Is.EqualTo("Tranquility"));
+		Assert.That(result.Edition, Is.EqualTo("premium"));
+		Assert.That(result.Role, Is.EqualTo("client"));
+		Assert.That(result.Aid, Is.EqualTo(false));
+		Assert.That(result.ResFromStuffOnly, Is.EqualTo(false));
+		Assert.That(result.Port, Is.EqualTo(26000));
 	}
 }
